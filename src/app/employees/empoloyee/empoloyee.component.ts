6c11f6c6535dbad21030945b83558e502743cf0f
Ihor ParkH <iuparhomenko@gmail.com>
2019-08-28 14:41:36 +0300

    start

---
+++ src/app/employees/empoloyee/empoloyee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empoloyee',
+  templateUrl: './empoloyee.component.html',
+  styleUrls: ['./empoloyee.component.scss']
+})
+export class EmpoloyeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
